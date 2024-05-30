c8c6f8f899aecdf4fcb32f8fb3b4db6c93d16a52
aivylv <lyvialarasati@gmail.com>
2024-05-30 11:55:24 +0700

    menginisialisasi dua variabel lokal

--- circularQueue/circularQueue.cpp
+++ circularQueue/circularQueue.cpp
@@ -58,9 +58,15 @@ public:
                 FRONT = FRONT + 1;
         }
     }
+
+    void display() {
+
+        int FRONT_Position = FRONT;
+        int REAR_Position = REAR;
+    }
 };
 
 int main()
 {
-    
+
 }
